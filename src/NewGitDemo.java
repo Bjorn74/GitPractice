5e19d057fc70732fdc5847637f81568f16af5c15
Bjorn74 <benfogt@gmail.com>
2018-01-23 14:20:10 -0500

    Fill Repo

---
+++ src/NewGitDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Please Enter your First Name: ");
+		String vName = scan.nextLine();
+		System.out.println();
+		System.out.println("Hello, " + vName + ".");
+		
+	}
+
+}
